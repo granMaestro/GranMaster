b93f8c0ca17578ba9ff90a1fc4afa05aacf6777e
fernando ortiz <uriel.ortiz@softonitg.com>
2017-06-08 07:16:07 -0500

    nueva configuracion

--- front/src/app/views/home/Home.js
+++ front/src/app/views/home/Home.js
@@ -2,43 +2,16 @@
 
 import React, {PureComponent } from 'react';
 // import PropTypes        from 'prop-types';
-import {Jumbotron}             from '../../components';
-import cx                      from 'classnames';
-import { Link }                from 'react-router';
 import FontAwesome             from 'react-fontawesome';
 
 
 
 class Home extends PureComponent {
-  state = {
-    animated: true,
-    viewEntersAnim: true
-  };
 
   render() {
-    const { animated, viewEntersAnim } = this.state;
     return(
-      <div
-        key="homeView"
-        className={cx({
-          'animatedViews': animated,
-          'view-enter': viewEntersAnim
-        })}>
-        <Jumbotron>
-          <h1>
-            comparte el amor por los libros papi
-          </h1>
-          <h2>
-            <div className="input-group">
-              <input type="text" className="form-control" aria-label="Amount (to the nearest dollar)" />
-              <span className="input-group-addon"><FontAwesome name='search'/></span>
-            </div>
-          </h2>
-          <h2>
-            Encuentra el libro que buscas entre miles de titulos ofertados por nuestra comunidad 
-          </h2>
-           
-        </Jumbotron>
+      <div>
+        BIENVENIDO A GRAN MASTER
       </div>
     );
   }
